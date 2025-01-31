cf8906a8531936ae67896627517acdae09aca6dc
ruslan_lukas <ru7stalker@gmail.com>
2025-01-31 17:39:22 +0200

    task5compl

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,12 @@
+const bTn=document.querySelector('.change-color');
+let spanColor=document.querySelector(".color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const x =`#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+    spanColor.textContent=x;
+    document.body.style.backgroundColor=x;
 }
+
+bTn.addEventListener('click', getRandomHexColor);
