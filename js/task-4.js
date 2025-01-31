bc177da5c73d0cc06e219d6ae873f53416bcd53b
ruslan_lukas <ru7stalker@gmail.com>
2025-01-31 14:00:08 +0200

    task4compl

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
 
+const registerForm = document.querySelector(".login-form");
+function dataBase(event) {
+    event.preventDefault();
+    const form = event.target;
+    let email = form.elements.email.value.trim();
+    let password = form.elements.password.value.trim();
+ 
+    if (email === "" || password === "") {
+        alert ("All form fields must be filled in");
+    }
+
+    const dataValid = {
+        "email" : email,
+        "password": password,
+    }
+    console.log(dataValid);
+    form.reset();
+
+}
+registerForm.addEventListener('submit', dataBase)
